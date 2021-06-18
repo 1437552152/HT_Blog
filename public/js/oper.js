4b162fe5886a507bc3b5f3543d996d45be5a0ae3
DESKTOP-G20IAAD\yyf <1437552152@qq.com>
2021-06-18 17:41:33 +0800

    提交

--- public/js/oper.js
+++ public/js/oper.js
@@ -1,5 +1,6 @@
 $(function(){
     $('.theme-login').click(function(){
+		debugger;
 		$('.theme-popover-mask').fadeIn(100);
 		$('.theme-popover').slideDown(200);
 	})
@@ -7,4 +8,24 @@ $(function(){
 		$('.theme-popover-mask').fadeOut(100);
 		$('.theme-popover').slideUp(200);
 	})
+
+
+	// 火箭图标
+    $(".backTop").on("click",function(){
+        $('html,body').animate({scrollTop:0},'slow');
+    });
+    function back_Top(){
+        if($(document).scrollTop() <=100){
+            $(".backTop").fadeOut();
+        }
+        else{
+            $(".backTop").fadeIn();
+        }
+    }
+    $(window).scroll(function(){
+        back_Top();
+    });
+    back_Top();
+
+
 })
--- routes/web.js
+++ routes/web.js
@@ -10,32 +10,81 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-var httpRequest =require('../util/http')
-// 字段说明
-//  isShow：0 表示展示      1 表示物理删除即隐藏
+var httpRequest = require('../util/http');
+var list= [{
+  title: '测试标题',
+  date: '2020-01-15 10:15',
+  descc: "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测",
+  tag: ['标签1', '标签2', '标签3', '标签4'],
+  viewNum: 203,
+  id: 1,
+  zanNum: 10,
+  commentNum: 10
+},
+{
+  title: '测试标题',
+  date: '2020-01-15 10:15',
+  descc: "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测",
+  tag: ['标签1', '标签2', '标签3', '标签4'],
+  viewNum: 203,
+  zanNum: 10,
+  id: 2,
+  commentNum: 10
+},
+{
+  title: '测试标题',
+  date: '2020-01-15 10:15',
+  descc: "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测",
+  tag: ['标签1', '标签2', '标签3', '标签4'],
+  viewNum: 203,
+  zanNum: 10,
+  id: 3,
+  commentNum: 10
+}
+]
 
-/****小程序的登录与注册***/
-router.get('/',(req,res)=>{
+
+
+// 首页
+router.get('/', (req, res) => {
   res.render('index', {
+    list
+  })
+});
+router.get('/index.html', (req, res) => {
+  res.render('index', {
+    list
+  })
+});
+// 意见反馈
+router.get('/feedback.html', (req, res) => {
+  res.render('feedback', {
+    list
+  })
+});
+
+// 服务宗旨
+router.get('/purpose.html', (req, res) => {
+  res.render('purpose', {
+    list
+  })
+});
+
+// 详情页
+router.get('/detail.html', (req, res) => {
+  res.render('detail', {
     data: {}
   })
 });
 
-router.get('/index.html',(req,res)=>{
-  res.render('index', {
-    data: {}
-  })
-});
 
-
-
-router.get('/1/:menu',(req,res)=>{
+router.get('/1/:menu', (req, res) => {
   res.render('two', {
     data: {}
   })
 });
 
-router.get('/a/:menu',(req,res)=>{
+router.get('/a/:menu', (req, res) => {
   res.render('three', {
     data: {}
   })
